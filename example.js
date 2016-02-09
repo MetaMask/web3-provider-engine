54171be272a2993eaaac8e29f361bd3b4d8e5d41
kumavis <aaron@kumavis.me>
2016-02-09 15:43:36 -0800

    breakout basic zero client

--- example.js
+++ example.js
@@ -1,58 +1,12 @@
 const ProviderEngine = require('./index.js')
-const CacheSubprovider = require('./subproviders/cache.js')
-const RpcSubprovider = require('./subproviders/rpc.js')
-const VmSubprovider = require('./subproviders/vm.js')
-const FilterSubprovider = require('./subproviders/filters.js')
-const DefaultFixture = require('./subproviders/default-fixture.js')
-const LightWalletSubprovider = require('./subproviders/lightwallet.js')
+const ZeroClientProvider = require('./zero.js')
 
-module.exports = zeroClientProvider
+// create engine
+var engine = ZeroClientProvider({
+  rpcUrl: 'https://testrpc.metamask.io/',
+})
 
-zeroClientProvider()
-
-function zeroClientProvider(opts){
-  opts = opts || {}
-
-  var engine = new ProviderEngine()
-
-  // cache layer
-  var cacheSubprovider = new CacheSubprovider()
-  engine.addProvider(cacheSubprovider)
-
-  // static
-  var staticSubprovider = new DefaultFixture()
-  engine.addProvider(staticSubprovider)
-
-  // filters
-  var filterSubprovider = new FilterSubprovider()
-  engine.addProvider(filterSubprovider)
-
-  // vm
-  var vmSubprovider = new VmSubprovider()
-  engine.addProvider(vmSubprovider)
-
-  // id mgmt
-  var idmgmtSubprovider = new LightWalletSubprovider()
-  engine.addProvider(idmgmtSubprovider)
-
-  // data source
-  var rpcSubprovider = new RpcSubprovider({
-    rpcUrl: opts.rpcUrl || 'https://testrpc.metamask.io/',
-  })
-  engine.addProvider(rpcSubprovider)
-
-  // log new blocks
-  engine.on('block', function(block){
-    // lazy hack - move caching and current block to engine
-    engine.currentBlock = block
-    console.log('================================')
-    console.log('BLOCK CHANGED:', '#'+block.number.toString('hex'), '0x'+block.hash.toString('hex'))
-    console.log('================================')
-  })
-
-  // start polling
-  engine.start()
-
-  return engine
-
-}
+// log new blocks
+engine.on('block', function(block){
+  console.log('BLOCK CHANGED:', '#'+block.number.toString('hex'), '0x'+block.hash.toString('hex'))
+})
---
+++ zero.js
@@ -0,0 +1,55 @@
+const ProviderEngine = require('./index.js')
+const CacheSubprovider = require('./subproviders/cache.js')
+const RpcSubprovider = require('./subproviders/rpc.js')
+const VmSubprovider = require('./subproviders/vm.js')
+const FilterSubprovider = require('./subproviders/filters.js')
+const DefaultFixture = require('./subproviders/default-fixture.js')
+const LightWalletSubprovider = require('./subproviders/lightwallet.js')
+
+module.exports = ZeroClientProvider
+
+
+function ZeroClientProvider(opts){
+  opts = opts || {}
+
+  var engine = new ProviderEngine()
+
+  // cache layer
+  var cacheSubprovider = new CacheSubprovider()
+  engine.addProvider(cacheSubprovider)
+
+  // static
+  var staticSubprovider = new DefaultFixture()
+  engine.addProvider(staticSubprovider)
+
+  // filters
+  var filterSubprovider = new FilterSubprovider()
+  engine.addProvider(filterSubprovider)
+
+  // vm
+  var vmSubprovider = new VmSubprovider()
+  engine.addProvider(vmSubprovider)
+
+  // id mgmt
+  var idmgmtSubprovider = new LightWalletSubprovider()
+  engine.addProvider(idmgmtSubprovider)
+
+  // data source
+  var rpcSubprovider = new RpcSubprovider({
+    rpcUrl: opts.rpcUrl || 'https://testrpc.metamask.io/',
+  })
+  engine.addProvider(rpcSubprovider)
+
+  // // log new blocks
+  // engine.on('block', function(block){
+  //   console.log('================================')
+  //   console.log('BLOCK CHANGED:', '#'+block.number.toString('hex'), '0x'+block.hash.toString('hex'))
+  //   console.log('================================')
+  // })
+
+  // start polling
+  engine.start()
+
+  return engine
+
+}
