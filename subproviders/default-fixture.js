b9f72587d9689bcf9f58ac60a8b895380064229b
kumavis <aaron@kumavis.me>
2016-04-26 10:42:26 -0700

    subp - default-fixture - remove some hardcoded 'net' methods

--- subproviders/default-fixture.js
+++ subproviders/default-fixture.js
@@ -1,5 +1,6 @@
 const inherits = require('util').inherits
 const FixtureProvider = require('./fixture.js')
+const version = require('../package.json').version
 
 module.exports = DefaultFixtures
 
@@ -8,11 +9,8 @@ inherits(DefaultFixtures, FixtureProvider)
 function DefaultFixtures() {
   const self = this
   var responses = {
-    web3_clientVersion: 'MetaMask-ZeroClient/v0.0.0/javascript',
-    net_version: '1',
+    web3_clientVersion: 'ProviderEngine/v'+version+'/javascript',
     net_listening: true,
-    net_peerCount: '0xc',
-    eth_protocolVersion: '63',
     eth_hashrate: '0x00',
     eth_mining: false,
     eth_syncing: true,
