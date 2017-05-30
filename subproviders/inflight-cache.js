b98da7b21ef78f5e0c14f799c5c529933116916b
Dan Finlay <dan@danfinlay.com>
2017-05-30 12:16:43 -0700

    Some cleanup

--- subproviders/inflight-cache.js
+++ subproviders/inflight-cache.js
@@ -12,33 +12,37 @@ class InflightCacheSubprovider extends Subprovider {
     this.engine = engine
   }
 
-  handleRequest (req, next, end) => {
+  handleRequest (req, next, end) {
     const cacheId = cacheIdentifierForPayload(req)
+
     // if not cacheable, skip
     if (!cacheId) return next()
+
     // check for matching requests
     let activeRequestHandlers = this.inflightRequests[cacheId]
+
+    if (!activeRequestHandlers) {
+      // setup response handler array for subsequent requests
+      activeRequestHandlers = []
+      this.inflightRequests[cacheId] = activeRequestHandlers
+
+      // allow request to be handled normally
+      next((done) => {
+        // clear inflight requests
+        delete this.inflightRequests[cacheId]
+        // complete this request
+        done(null, )
+        // once request has been handled, call all waiting handlers
+        activeRequestHandlers.forEach((handler) => handler(res))
+      })
     // if found, wait for the active request to be handled
-    if (activeRequestHandlers) {
+    } else {
       // setup the response lister
       activeRequestHandlers.push((handledRes) => {
         const res = {}
         res.result = handledRes.result
         res.error = handledRes.error
         end(null, res)
-      })
-    } else {
-      // setup response handler array for subsequent requests
-      activeRequestHandlers = []
-      this.inflightRequests[cacheId] = activeRequestHandlers
-      // allow request to be handled normally
-      next((done) => {
-        // clear inflight requests
-        delete this.inflightRequests[cacheId]
-        // complete this request
-        done()
-        // once request has been handled, call all waiting handlers
-        activeRequestHandlers.forEach((handler) => handler(res))
       })
     }
   }
