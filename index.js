c25156d618a1d9712d5f401ab67b6e93fbe3dddb
kumavis <aaron@kumavis.me>
2017-12-13 09:46:18 -0800

    opts - specify blockTrackerProvider via opts.blockTrackerProvider

--- index.js
+++ index.js
@@ -20,9 +20,10 @@ function Web3ProviderEngine(opts) {
   // parse options
   opts = opts || {}
   // block polling
-  const skipInitBlockProvider = { sendAsync: self._handleAsync.bind(self) }
+  const skipInitLockProvider = { sendAsync: self._handleAsync.bind(self) }
+  const blockTrackerProvider = opts.blockTrackerProvider || skipInitLockProvider
   self._blockTracker = new EthBlockTracker({
-    provider: skipInitBlockProvider,
+    provider: blockTrackerProvider,
     pollingInterval: opts.pollingInterval || 4000,
   })
   // handle new block
