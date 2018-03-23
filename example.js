91922f481b791d0e897a2eee0e81bcd684874df7
Ryan Rowland <ryan.w.rowland@gmail.com>
2018-03-23 09:35:46 -0700

    Issue 189 | Add WebsocketProvider

--- example.js
+++ example.js
@@ -1,13 +1,27 @@
 const ProviderEngine = require('./index.js')
 const ZeroClientProvider = require('./zero.js')
+const FetchProvider = require('./subproviders/fetch')
+
+const fetchEngine = ZeroClientProvider({
+  getAccounts: function(){},
+  dataSubprovider: new FetchProvider({
+    rpcUrl: 'https://mainnet.infura.io',
+  })
+})
 
 // create engine
-const engine = ZeroClientProvider({
+const websocketEngine = ZeroClientProvider({
   getAccounts: function(){},
-  rpcUrl: 'https://mainnet.infura.io/',
+  rpcUrl: 'wss://mainnet.infura.io/_ws',
+  //debug: true,
 })
 
 // log new blocks
-engine.on('block', function(block){
-  console.log('BLOCK CHANGED:', '#'+block.number.toString('hex'), '0x'+block.hash.toString('hex'))
+websocketEngine.on('block', function(block) {
+  console.log('WS:', '#'+block.number.toString('hex'), '0x'+block.hash.toString('hex'))
+})
+
+// log new blocks
+fetchEngine.on('block', function(block) {
+  console.log('Fetch:', '#'+block.number.toString('hex'), '0x'+block.hash.toString('hex'))
 })
