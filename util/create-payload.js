a51673171f06739c4793f2c2219b46202dd3a924
Carsten Munk <carsten.munk@zipperglobal.com>
2018-04-05 04:20:55 +0200

    Still send jsonrpc header
    Signed-off-by: Carsten Munk <carsten.munk@zipperglobal.com>

--- util/create-payload.js
+++ util/create-payload.js
@@ -4,10 +4,11 @@ const extend = require('xtend')
 module.exports = createPayload
 
 
-function createPayload(data) {
+function createPayload(data){
   return extend({
     // defaults
     id: getRandomId(),
+    jsonrpc: '2.0',
     params: [],
     // user-specified
   }, data)
