ff84935f4f7713128dc63180e42b395df9defa08
kumavis <aaron@kumavis.me>
2020-05-26 15:27:00 +0800

    simply random-id

--- util/random-id.js
+++ util/random-id.js
@@ -1,14 +1,7 @@
-// gotta keep it within MAX_SAFE_INTEGER
-const extraDigits = 3
-
 module.exports = createRandomId
 
 
-function createRandomId(){
-  // 13 time digits
-  var datePart = new Date().getTime()*Math.pow(10, extraDigits)
-  // 3 random digits
-  var extraPart = Math.floor(Math.random()*Math.pow(10, extraDigits))
-  // 16 digits
-  return datePart+extraPart
+function createRandomId () {
+  // random id
+  return Math.floor(Number.MAX_SAFE_INTEGER * Math.random())
 }
