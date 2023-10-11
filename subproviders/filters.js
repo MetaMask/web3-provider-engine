f5a78a952881febdae4179ff908821e908b4a3b7
legobeat <109787230+legobeat@users.noreply.github.com>
2023-10-11 23:05:43 +0000

    fix(suproviders/filters): Rename class from SubsciptionsSubProvider to FiltersSubProvider (#442)

--- subproviders/filters.js
+++ subproviders/filters.js
@@ -1,12 +1,12 @@
 const ProviderSubprovider = require('./json-rpc-engine-middleware')
 const createFilterMiddleware = require('eth-json-rpc-filters')
 
-class SubscriptionsSubprovider extends ProviderSubprovider {
+class FiltersSubprovider extends ProviderSubprovider {
   constructor() {
-    super(({ blockTracker, provider, engine }) => {
+    super(({ blockTracker, provider }) => {
       return createFilterMiddleware({ blockTracker, provider })
     })
   }
 }
 
-module.exports = SubscriptionsSubprovider
+module.exports = FiltersSubprovider
