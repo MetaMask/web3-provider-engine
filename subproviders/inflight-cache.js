55c3767d524f26c2f24632ceac812eaaa73781f6
legobt <6wbvkn0j@anonaddy.me>
2024-04-18 03:58:14 +0000

    deps: eth-json-rpc-middleware@^6.0.0->^8.1.0

--- subproviders/inflight-cache.js
+++ subproviders/inflight-cache.js
@@ -1,5 +1,5 @@
 const ProviderSubprovider = require('./json-rpc-engine-middleware')
-const createInflightCacheMiddleware = require('eth-json-rpc-middleware/inflight-cache')
+const { createInflightCacheMiddleware } = require('eth-json-rpc-middleware/dist/inflight-cache')
 
 class InflightCacheSubprovider extends ProviderSubprovider {
   constructor(opts) {
